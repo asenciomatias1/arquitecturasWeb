37b26563cdc6f086966f8a188d82996ee4721a35
FedeBayer <89879671+FedeBayer@users.noreply.github.com>
2022-11-14 16:10:16 -0300

    Added swagger annotations to estudianteController

--- src/main/java/com/example/demo/controller/EstudianteControllerJpa.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerJpa.java
@@ -26,6 +26,12 @@ public class EstudianteControllerJpa {
         this.service = service;
     }
 
+    @ApiOperation(value = "Get list of estudiantes", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!")})
     @GetMapping("")
     public Iterable<Estudiante> getEstudiantes() {
         return service.getAllEstudiantes();
@@ -42,29 +48,43 @@ public class EstudianteControllerJpa {
         return service.getEstudiantesOrderByApellido();
     }
 
+    @ApiOperation(value = "Save a student", response = Estudiante.class)
     @PostMapping("")
     public Estudiante saveEstudiante(@RequestBody Estudiante e) {
         return service.saveEstudiante(e);
     }
 
+    @ApiOperation(value = "Get list of estudiantes by genre ", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!")})
     @GetMapping("/genero/{genero}")
     public List<Estudiante> getEstudiantesByGenero(@PathVariable(value="genero") String genero) {
         return service.findAllByGenero(genero);
     }
 
+    @ApiOperation(value = "Get list of estudiantes by career id and city name ", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!")})
     @GetMapping("/carrera/{idCarrera}/ciudad/{ciudad}")
-    public List<Estudiante> getEstudiantesByGenero(@PathVariable(value="idCarrera")
+    public List<Estudiante> getEstudiantesByCiudadAndCarrera(@PathVariable(value="idCarrera")
     	long idCarrera, @PathVariable(value="ciudad") String ciudad) {
 
         return service.findAllByCarreraByCiudad(idCarrera, ciudad);
     }
 
+    @ApiOperation(value = "Get one student by id", response = Estudiante.class)
     @GetMapping("/{id}")
     Optional<Estudiante> one(@PathVariable long id) {
         return service.getEstudianteById(id);
     }
 
-
+     @ApiOperation(value = "Replace one student by id", response = Estudiante.class)
     @PutMapping("/{id}")
     Estudiante replaceEstudiante(@RequestBody Estudiante newEstudiante, @PathVariable long id) {
 
@@ -84,6 +104,7 @@ public class EstudianteControllerJpa {
                 });
     }
 
+    @ApiOperation(value = "Delete one student by id")
     @DeleteMapping("/{id}")
     void deleteEstudiante(@PathVariable long id) {
         service.deleteEstudianteById(id);
